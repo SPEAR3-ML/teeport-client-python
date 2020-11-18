231e13b2355cbdac9c23d30488f11675c1a5cf4d
Zhe Zhang <wenatuhs@gmail.com>
2020-11-18 01:23:53 -0800

    Remove the autoapi extension and use the rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -29,12 +29,8 @@ author = 'Zhe Zhang'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-#     'autoapi.extension'
+    'sphinx.ext.napoleon'
 ]
-
-# Set up autoapi
-autoapi_dirs = ['../teeport']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -50,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
