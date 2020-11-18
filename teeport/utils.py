974394b81416b9817daf5759459d80b7ae2996da
Zhe Zhang <wenatuhs@gmail.com>
2020-11-18 01:22:21 -0800

    Pass the docstrings to sphinx for the methods with decorators

--- teeport/utils.py
+++ teeport/utils.py
@@ -4,11 +4,13 @@
 import asyncio
 import inspect
 import urllib.parse
+from functools import wraps
 
 def make_async(func):
     if inspect.iscoroutinefunction(func):
         return func
     else:
+        @wraps(func)
         async def func_a(*args, **kwargs):
             await asyncio.sleep(0)
             return func(*args, **kwargs)
@@ -17,6 +19,7 @@ def make_async(func):
 
 def make_sync(func):
     if inspect.iscoroutinefunction(func):
+        @wraps(func)
         def func_d(*args, **kwargs):
             try:
                 res = asyncio.get_event_loop().run_until_complete(func(*args, **kwargs))
