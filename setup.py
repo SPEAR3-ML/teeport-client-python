072f2f2d427573bd92b826195c2c2329b2b9fdf5
Zhe Zhang <wenatuhs@gmail.com>
2020-11-25 23:41:35 -0800

    Add nest_asyncio as a dependence Since we don't use multi-threading, we'll have to make use of the nest_asyncio package

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     install_requires=[
         'numpy',
         'websockets',
+        'nest_asyncio',
         'anytree'
     ],
     classifiers=[
--- teeport/__init__.py
+++ teeport/__init__.py
@@ -1,4 +1,10 @@
 # Copyright (c) 2019-2020, SPEAR3 authors (see AUTHORS.txt).
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
 
+# The reason we need nest_asyncio is that we run multiple event loops in a
+# single thread, that's not allowed. The future work should be create a new
+# thread for each event loop so we can get rid of nest_asyncio
+import nest_asyncio
+nest_asyncio.apply()
+
 from .teeport import Teeport
